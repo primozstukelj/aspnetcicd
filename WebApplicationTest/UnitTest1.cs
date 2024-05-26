4db05b0629938d927fc1249d1876d824de8abb3f
Primoz Stukelj <primoz.stukelj@inova.si>
2024-05-26 09:50:03 +0200

    Added xunit test

---
+++ WebApplicationTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace WebApplicationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // test passes
+            Assert.True(true);
+        }
+    }
+}
